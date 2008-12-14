e7a0654092dd325c87739833474924d42b475949
Dan Dudley <born2snipe@gmail.com>
2008-12-13 20:39:04 -0600

    added mockito created CommandExecutor

---
+++ src/main/java/git4idea/commands/CommandExecutor.java
@@ -0,0 +1,64 @@
+package git4idea.commands;
+
+import com.intellij.openapi.vcs.VcsException;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+
+
+public class CommandExecutor {
+
+    /**
+     * Shell out a command and capture the output
+     *
+     * @param workingDirectory - the directory to execute the command from
+     * @param command          - the command to be executed
+     * @return the output from executing the given command
+     * @throws VcsException when a problem occurs while executing the given command
+     */
+    public String execute(File workingDirectory, String command) throws VcsException {
+        StringBuilder builder = new StringBuilder();
+        BufferedInputStream input = null;
+        try {
+            Process process = createProcess(workingDirectory, command);
+            input = new BufferedInputStream(process.getInputStream());
+            ByteArrayOutputStream output = new ByteArrayOutputStream();
+            int length = 0;
+            byte[] buffer = new byte[16 * 1024];
+            while ((length = input.read(buffer)) != -1) {
+                output.write(buffer, 0, length);
+                output.flush();
+            }
+            builder.append(new String(output.toByteArray()));
+            if (process.waitFor() != 0) {
+                throw new VcsException(builder.toString());
+            }
+        } catch (InterruptedException e) {
+            throw new VcsException(builder.toString(), e);
+        } catch (IOException e) {
+            throw new VcsException(e);
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+
+                }
+            }
+        }
+        return builder.toString();
+    }
+
+    protected Process createProcess(File workingDirectory, String command) throws VcsException {
+        ProcessBuilder pb = new ProcessBuilder(command);
+        pb.redirectErrorStream(true);
+        pb.directory(workingDirectory);
+        try {
+            return pb.start();
+        } catch (IOException e) {
+            throw new VcsException(e);
+        }
+    }
+}
---
+++ src/test/java/git4idea/commands/CommandExecutorTest.java
@@ -0,0 +1,110 @@
+package git4idea.commands;
+
+import com.intellij.openapi.vcs.VcsException;
+import junit.framework.TestCase;
+import static org.mockito.Mockito.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+
+public class CommandExecutorTest extends TestCase {
+    private Process process;
+    private ShuntCommandExecutor executor;
+
+    public void test_execute_ReadingOutputThrowsIOException() throws InterruptedException, VcsException {
+        IOException cause = new IOException();
+
+        when(process.getInputStream()).thenReturn(new ExplodingInputStream(cause));
+
+        try {
+            executor.execute(new File("."), "ls");
+            fail();
+        } catch (VcsException err) {
+            assertEquals(cause, err.getCause());
+        }
+
+        verify(process).getInputStream();
+    }
+
+    public void test_execute_WaitForThrowsInterruptedException() throws InterruptedException, VcsException {
+        String expectedOutput = "Output";
+        InterruptedException cause = new InterruptedException("You have been interrupted!");
+
+        when(process.waitFor()).thenThrow(cause);
+        when(process.getInputStream()).thenReturn(new ByteArrayInputStream(expectedOutput.getBytes()));
+
+        try {
+            executor.execute(new File("."), "ls");
+            fail();
+        } catch (VcsException err) {
+            assertEquals(expectedOutput, err.getMessage());
+            assertEquals(cause, err.getCause());
+        }
+
+        verify(process).waitFor();
+        verify(process).getInputStream();
+    }
+
+    public void test_execute_ReturnCodeZero() throws InterruptedException, VcsException {
+        String expectedOutput = "Output";
+
+        when(process.waitFor()).thenReturn(0);
+        when(process.getInputStream()).thenReturn(new ByteArrayInputStream(expectedOutput.getBytes()));
+
+        String output = executor.execute(new File("."), "ls");
+
+        assertNotNull(output);
+        assertEquals(expectedOutput, output);
+
+        verify(process).waitFor();
+        verify(process).getInputStream();
+    }
+
+    public void test_execute_ReturnCodeNotZero() throws InterruptedException {
+        String expectedOutput = "Output";
+
+        when(process.waitFor()).thenReturn(-1);
+        when(process.getInputStream()).thenReturn(new ByteArrayInputStream(expectedOutput.getBytes()));
+
+        try {
+            executor.execute(new File("."), "ls");
+            fail();
+        } catch (VcsException e) {
+            assertEquals(expectedOutput, e.getMessage());
+        }
+
+        verify(process).waitFor();
+        verify(process).getInputStream();
+    }
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        process = mock(Process.class);
+        executor = new ShuntCommandExecutor();
+        executor.process = process;
+    }
+
+    private static class ShuntCommandExecutor extends CommandExecutor {
+        private Process process;
+
+        @Override
+        protected Process createProcess(File workingDirectory, String command) {
+            return process;
+        }
+    }
+
+    private static class ExplodingInputStream extends InputStream {
+        private IOException error;
+
+        private ExplodingInputStream(IOException error) {
+            this.error = error;
+        }
+
+        public int read() throws IOException {
+            throw error;
+        }
+    }
+}
